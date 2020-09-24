de440aa7f5270b10c84cec02aa7f3f8ac625c892
Lucas Borges <lucas.borges@supermercadonow.com>
2020-09-24 05:36:02 -0300

    fix server

---
+++ build/precache-manifest.b58fa8f1b13783b460e09c0848afb1f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca17d2cf15fbafe4ba75341b778f5095",
+    "url": "/index.html"
+  },
+  {
+    "revision": "985abfe44278dbc85ef8",
+    "url": "/static/css/2.83518d3f.chunk.css"
+  },
+  {
+    "revision": "985abfe44278dbc85ef8",
+    "url": "/static/js/2.788fafa1.chunk.js"
+  },
+  {
+    "revision": "d64d84a7df068bcf8e3a45ce05d02f00",
+    "url": "/static/js/2.788fafa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "073b3afb0cfda8b64833",
+    "url": "/static/js/main.3e414438.chunk.js"
+  },
+  {
+    "revision": "c0419ae33e70a14e4ea4",
+    "url": "/static/js/runtime-main.25b7594c.js"
+  }
+]);
